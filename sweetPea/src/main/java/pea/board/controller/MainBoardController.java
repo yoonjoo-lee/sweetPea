58eb3db0b2324fa284065a0ae18817ff9a5d5e81
yoonjoo-lee <yjcandoit@gmail.com>
2022-07-07 17:47:54 +0900

    mainboard ~

--- sweetPea/src/main/java/pea/board/controller/MainBoardController.java
+++ sweetPea/src/main/java/pea/board/controller/MainBoardController.java
@@ -1,6 +1,10 @@
 package pea.board.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -26,14 +30,32 @@ public class MainBoardController {
 		return "mainboard/list";
 	}
 	
-	@RequestMapping(value="mainboard/write.do")
-	public String write(HttpServletRequest request, HttpSession session) {
-		session = request.getSession();
+	@RequestMapping(value="mainboard/home.do")
+	public String home() {
 		
-//		UserVo login = (UserVo)session.getAttribute("login");
-//		
-//		System.out.println("login uinx"+ login.getIudx());
-		
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="mainboard/gotowrite.do")
+	public String gotowrite(int uidx) {
 		return "mainboard/write";
+	}
+	
+	@RequestMapping(value="mainboard/write.do")
+	public void write(HttpServletResponse response, HttpServletRequest request, HttpSession session) throws IOException {
+		session = request.getSession();
+		PrintWriter pw = response.getWriter();
+		
+		UserVo login = (UserVo)session.getAttribute("login");
+		if (login==null) {
+			pw.append("<script>alert('GO TO LOGIN .');location.href='home.do'</script>"); // 다른페이지로 넘어가야하기에 redirect는 먹히지 않기에 .do로 보내라.
+			pw.flush();
+		}else {
+			pw.append("<script>location.href='gotowrite.do?uidx="+login.getUidx()+"';</script>"); // 다른페이지로 넘어가야하기에 redirect는 먹히지 않기에 .do로 보내라.
+			pw.flush();
+		}
+		System.out.println("login uinx"+ login.getUidx());
+		
+		//return "mainboard/write";
 	}
 }
