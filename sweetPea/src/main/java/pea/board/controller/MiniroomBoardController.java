69ca7736aec248f3a712fc320fbb04e80f18bb6c
piomia <107603768+piomia@users.noreply.github.com>
2022-08-16 09:11:23 +0900

    0816-0911

--- sweetPea/src/main/java/pea/board/controller/MiniroomBoardController.java
+++ sweetPea/src/main/java/pea/board/controller/MiniroomBoardController.java
@@ -56,11 +56,10 @@ public class MiniroomBoardController {
 		}
 		session.setAttribute("mini", vo);
     
-		if (mobile1 || mobile2) {
-			session.setAttribute("device", "MOBILE");
-		    return "minihome/main-m";
+		String device = (String) session.getAttribute("device");
+		if(device == "MOBILE") {
+			return "minihome/main-m";
 		} else {
-			session.setAttribute("device", "PC");
 			return "minihome/main";
 		}
 	}
